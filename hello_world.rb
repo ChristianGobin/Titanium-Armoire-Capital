008b084837bf92329408565050cefa4263406958
Christian G <chrisgobin94@gmail.com>
2021-08-13 12:23:35 -0400

    Update hello_world.rb

--- hello_world.rb
+++ hello_world.rb
@@ -1,9 +1,18 @@
 puts "Hello World!"
 print "Hello World!"
 
-# string variable
-first_name = "Chris"
+# int and float variables
+agef = 90.0
 age = 90
+
+# Getting user input
+print "Enter Your Name: "
+name = gets
+
+# Getting int
+print "Enter Your Age: "
+age = gets.to_i
+
 
 # interpolation
 puts "Hello #{first_name} you are #{age} years old!"
